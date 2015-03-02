d7f60fe52afdf60f5884bc1393c9e3a980a579ef
Alvaro Portela <alvaropfn@gmail.com>
2015-03-01 22:48:46 -0300

    add patoMonstro

---
+++ patoMonstro.py
@@ -0,0 +1,9 @@
+from pato import *
+
+class patoMonstro(pato):
+    
+    def __init__(this):
+        pato.__init__(this)
+        this.voo = 'haaarrrrr'
+        this.nado = 'hurrrrr gulp'
+        
